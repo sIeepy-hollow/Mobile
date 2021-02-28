74e2e11184a2add5f661377659db3ceba52fb6cd
Aplyn <aplatyna@iMac-Aplyn.local>
2021-02-28 08:42:28 -0800

    first commit

---
+++ LABKA1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LABKA1
+//
+//  Created by Aplyn on 28.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
